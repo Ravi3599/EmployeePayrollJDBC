dec5cebaa7efc6047d9aee216a222fde59370af5
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-23 10:18:41 +0530

    UPDATED : ability to update salary

--- src/main/java/com/bridgelabz/employeepayrolljdbcapp/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/employeepayrolljdbcapp/EmployeePayrollDBService.java
@@ -2,15 +2,27 @@ package com.bridgelabz.employeepayrolljdbcapp;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mysql.cj.xdevapi.Statement;
 
 public class EmployeePayrollDBService {
+	private PreparedStatement employeePayrollDataStatement;
+	private static EmployeePayrollDBService employeePayrollDBService;
+
+	private EmployeePayrollDBService() {
+		
+	}
+	
+	public static EmployeePayrollDBService getInstance() {
+		if(employeePayrollDBService == null)
+			employeePayrollDBService = new EmployeePayrollDBService();
+		return employeePayrollDBService;
+	}
 	
 	private Connection getConnection() throws SQLException {
 		
@@ -25,7 +37,40 @@ public class EmployeePayrollDBService {
 		
 		return connection;
 	}
-	
+	private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet) {
+		
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		
+		try {
+			while(resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				double basicSalary = resultSet.getDouble("salary");
+				LocalDate startDate = resultSet.getDate("start").toLocalDate();
+				employeePayrollList.add(new EmployeePayrollData(id, name, basicSalary, startDate));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollList;
+		
+	}
+	public List<EmployeePayrollData> getEmployeePayrollData(String name) {
+		
+		List<EmployeePayrollData> employeePayrollList = null;
+		if(this.employeePayrollDataStatement == null)
+			this.prepareStatementForEmployeeData();
+		try {
+			employeePayrollDataStatement.setString(1,name);
+			ResultSet resultSet = employeePayrollDataStatement.executeQuery();
+			employeePayrollList = this.getEmployeePayrollData(resultSet);	
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollList;
+	}
 	public List<EmployeePayrollData> readData(){
 		
 		String sqlStatement = "SELECT emp_id, emp_name, basic_pay, start FROM employee JOIN payroll ON employee.payroll_id = payroll.payroll_id;";
@@ -44,9 +89,37 @@ public class EmployeePayrollDBService {
 			}
 		}
 		catch(SQLException e){
-			
 			e.printStackTrace();
 		}
 		return employeePayrollList;
+	}
+	public int updateEmployeeData(String name, double salary) {
+		
+		return this.updateEmployeeDataUsingStatement(name,salary);
+	}
+
+	private int updateEmployeeDataUsingStatement(String name, double salary) {
+		
+		String sqlStatement = String.format("UPDATE payroll ,employee SET net_pay = %2f WHERE employee.payroll_id = payroll.payroll_id AND emp_name = '%s';", salary, name);
+		
+		try (Connection connection = getConnection()){
+			java.sql.Statement statement = connection.createStatement();
+			return statement.executeUpdate(sqlStatement);
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}		
+		return 0;
+	}
+	private void prepareStatementForEmployeeData() {
+		
+		try {
+			Connection connection = this.getConnection();
+			String sqlStatement = "SELECT * FROM employee,payroll WHERE employee.payroll_id = payroll.payroll.id AND name = ?;";
+			employeePayrollDataStatement = connection.prepareStatement(sqlStatement);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
